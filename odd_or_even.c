23264067b593b9d7d6f73607e2d516791f9f4aeb
nirmalsharan08 <nirmalsharan08@users.noreply.github.com>
2026-07-04 14:53:04 +0530

    Add C programs and workshop files

---
+++ odd_or_even.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ int a;
+ printf("Enter a number: ");
+ scanf("%d", &a);
+if (a%2 == 0)
+    {printf("a is even");}
+else
+    {printf("a is odd");}
+}
---
+++ positive_or_negative.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter value: ");
+    scanf("%d",&n);
+    if (n>0)
+        {
+            printf("number is positive");
+        }
+        else
+        {
+            printf ("number is negative");
+        }
+      
+
+}
